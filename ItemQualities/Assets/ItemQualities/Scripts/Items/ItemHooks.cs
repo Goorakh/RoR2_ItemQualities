b38ea0234b2ab36b8dd9b507dde97349b90a53ca
Gorakh <petter.ohlen@gmail.com>
2025-09-22 10:10:55 +0200

    Add item group hooks for item displays & footstep events

--- ItemQualities/Assets/ItemQualities/Scripts/Items/ItemHooks.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/ItemHooks.cs
@@ -5,6 +5,7 @@ using MonoMod.Cil;
 using MonoMod.Utils;
 using RoR2;
 using System;
+using System.Collections;
 
 namespace ItemQualities.Items
 {
@@ -14,6 +15,21 @@ namespace ItemQualities.Items
         static void Init()
         {
             IL.RoR2.CharacterBody.RecalculateStats += CombineGroupedItemCountsPatch;
+            IL.RoR2.CharacterModel.UpdateItemDisplay += CombineGroupedItemCountsPatch;
+            IL.RoR2.FootstepHandler.Footstep_string_GameObject += CombineGroupedItemCountsPatch;
+
+            On.RoR2.CharacterMaster.HighlightNewItem += CharacterMaster_HighlightNewItem;
+        }
+
+        static IEnumerator CharacterMaster_HighlightNewItem(On.RoR2.CharacterMaster.orig_HighlightNewItem orig, CharacterMaster self, ItemIndex itemIndex)
+        {
+            ItemQualityGroup itemGroup = QualityCatalog.GetItemQualityGroup(QualityCatalog.FindItemQualityGroupIndex(itemIndex));
+            if (itemGroup)
+            {
+                itemIndex = itemGroup.BaseItemIndex;
+            }
+
+            return orig(self, itemIndex);
         }
 
         static void CombineGroupedItemCountsPatch(ILContext il)
