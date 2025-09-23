b1137698f09926572568e84b9101349e2bfdac2e
Gorakh <petter.ohlen@gmail.com>
2025-09-23 10:11:32 +0200

    Maintain quality when using printers/cauldrons

---
+++ ItemQualities/Assets/ItemQualities/Scripts/ItemCostQualityPatch.cs
@@ -0,0 +1,94 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+using static ItemQualities.ItemQualitiesContent;
+
+namespace ItemQualities
+{
+    static class ItemCostQualityPatch
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            IL.RoR2.ShopTerminalBehavior.DropPickup += CopyItemQualityFromCost;
+        }
+
+        static void CopyItemQualityFromCost(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(MoveType.After,
+                               x => x.MatchLdfld<ShopTerminalBehavior>(nameof(ShopTerminalBehavior.pickupIndex))))
+            {
+                Log.Error("Failed to find patch location");
+                return;
+            }
+
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Func<PickupIndex, ShopTerminalBehavior, PickupIndex>>(getAppropriateQualityDrop);
+
+            static PickupIndex getAppropriateQualityDrop(PickupIndex pickupIndex, ShopTerminalBehavior shopTerminalBehavior)
+            {
+                if (!shopTerminalBehavior.TryGetComponent(out ObjectPurchaseContext purchaseContext) || purchaseContext.PickupIndicesSpentOnLastPurchase.Length == 0)
+                    return pickupIndex;
+
+                float averageInputQualityTierValue = 0f;
+                int numInputItemsWithQuality = 0;
+
+                QualityTier minInputQualityTier = QualityTier.Count;
+
+                foreach (PickupIndex inputPickupIndex in purchaseContext.PickupIndicesSpentOnLastPurchase)
+                {
+                    QualityTier qualityTier = QualityCatalog.GetQualityTier(inputPickupIndex);
+
+                    averageInputQualityTierValue += (float)qualityTier;
+
+                    if (qualityTier > QualityTier.None)
+                    {
+                        numInputItemsWithQuality++;
+                    }
+
+                    if (qualityTier < minInputQualityTier)
+                    {
+                        minInputQualityTier = qualityTier;
+                    }
+                }
+
+                averageInputQualityTierValue /= purchaseContext.PickupIndicesSpentOnLastPurchase.Length;
+                QualityTier outputQualityTier = (QualityTier)Mathf.Clamp(Mathf.CeilToInt(averageInputQualityTierValue), 0, (int)QualityTier.Count - 1);
+
+                Log.Debug($"{numInputItemsWithQuality}/{purchaseContext.PickupIndicesSpentOnLastPurchase.Length} input items of quality (avg={outputQualityTier})");
+
+                if (outputQualityTier > QualityTier.None)
+                {
+                    bool shouldUpgradeQualityTier = true;
+                    if (numInputItemsWithQuality < purchaseContext.PickupIndicesSpentOnLastPurchase.Length)
+                    {
+                        // If at least 1 regular item in the input, reduce chance of upgrading the quality of the output
+                        float inputItemsWithQualityFraction = (float)numInputItemsWithQuality / purchaseContext.PickupIndicesSpentOnLastPurchase.Length;
+                        shouldUpgradeQualityTier = shopTerminalBehavior.rng.nextNormalizedFloat < Mathf.Pow(inputItemsWithQualityFraction, 3f);
+                    }
+
+                    if (shouldUpgradeQualityTier)
+                    {
+                        PickupIndex qualityPickupIndex = QualityCatalog.GetPickupIndexOfQuality(pickupIndex, outputQualityTier);
+
+                        if (qualityPickupIndex != PickupIndex.none && qualityPickupIndex != pickupIndex)
+                        {
+                            Log.Debug($"Upgraded tier of {pickupIndex}: {qualityPickupIndex}");
+                            pickupIndex = qualityPickupIndex;
+                        }
+                        else
+                        {
+                            Log.Warning($"Pickup {pickupIndex} is missing quality variant {outputQualityTier}");
+                        }
+                    }
+                }
+
+                return pickupIndex;
+            }
+        }
+    }
+}
---
+++ ItemQualities/Assets/ItemQualities/Scripts/ObjectPurchaseContext.cs
@@ -0,0 +1,41 @@
+﻿using HG;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ItemQualities
+{
+    public class ObjectPurchaseContext : MonoBehaviour
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            On.RoR2.CostTypeDef.PayCost += CostTypeDef_PayCost;
+        }
+
+        static CostTypeDef.PayCostResults CostTypeDef_PayCost(On.RoR2.CostTypeDef.orig_PayCost orig, CostTypeDef self, int cost, Interactor activator, GameObject purchasedObject, Xoroshiro128Plus rng, ItemIndex avoidedItemIndex)
+        {
+            CostTypeDef.PayCostResults payCostResults = orig(self, cost, activator, purchasedObject, rng, avoidedItemIndex);
+
+            List<PickupIndex> pickupIndicesSpentOnPurchase = new List<PickupIndex>(payCostResults.itemsTaken.Count + payCostResults.equipmentTaken.Count);
+
+            foreach (ItemIndex itemIndex in payCostResults.itemsTaken)
+            {
+                pickupIndicesSpentOnPurchase.Add(PickupCatalog.FindPickupIndex(itemIndex));
+            }
+
+            foreach (EquipmentIndex equipmentIndex in payCostResults.equipmentTaken)
+            {
+                pickupIndicesSpentOnPurchase.Add(PickupCatalog.FindPickupIndex(equipmentIndex));
+            }
+
+            ObjectPurchaseContext purchaseContext = purchasedObject.EnsureComponent<ObjectPurchaseContext>();
+            purchaseContext.PickupIndicesSpentOnLastPurchase = pickupIndicesSpentOnPurchase.ToArray();
+
+            return payCostResults;
+        }
+
+        public PickupIndex[] PickupIndicesSpentOnLastPurchase = Array.Empty<PickupIndex>();
+    }
+}
