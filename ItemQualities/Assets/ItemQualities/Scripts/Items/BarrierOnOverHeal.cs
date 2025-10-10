237dec7fcfd610c10d4ca339c19ba077d770ef51
Gorakh <petter.ohlen@gmail.com>
2025-10-11 01:49:41 +0200

    Add Aegis qualities

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/BarrierOnOverHeal.cs
@@ -0,0 +1,31 @@
+﻿using R2API;
+using RoR2;
+
+namespace ItemQualities.Items
+{
+    static class BarrierOnOverHeal
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            RecalculateStatsAPI.GetStatCoefficients += getStatCoefficients;
+        }
+
+        static void getStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
+        {
+            if (!sender || !sender.healthComponent)
+                return;
+
+            Inventory inventory = sender.inventory;
+
+            ItemQualityCounts barrierOnOverHeal = ItemQualitiesContent.ItemQualityGroups.BarrierOnOverHeal.GetItemCounts(inventory);
+            if (barrierOnOverHeal.TotalQualityCount > 0 && sender.healthComponent.barrier > 0f)
+            {
+                args.armorAdd += (20 * barrierOnOverHeal.UncommonCount) +
+                                 (50 * barrierOnOverHeal.RareCount) +
+                                 (80 * barrierOnOverHeal.EpicCount) +
+                                 (100 * barrierOnOverHeal.LegendaryCount);
+            }
+        }
+    }
+}
