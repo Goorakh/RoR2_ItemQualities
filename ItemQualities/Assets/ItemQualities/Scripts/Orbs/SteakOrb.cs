10e443451afde32cbd2dce15d8240b19b927cab6
Goorakh <petter.ohlen@gmail.com>
2025-10-11 18:54:32 +0200

    Steak visuals pass (#8)
    Add orb that gives the health bonus, like infusion

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Orbs/SteakOrb.cs
@@ -0,0 +1,59 @@
+﻿using RoR2;
+using RoR2.Orbs;
+using UnityEngine;
+
+namespace ItemQualities.Orbs
+{
+    public class SteakOrb : Orb
+    {
+        static EffectIndex _orbEffectIndex = EffectIndex.Invalid;
+
+        [SystemInitializer(typeof(EffectCatalogUtils))]
+        static void Init()
+        {
+            _orbEffectIndex = EffectCatalogUtils.FindEffectIndex("InfusionOrbEffect");
+            if (_orbEffectIndex == EffectIndex.Invalid)
+            {
+                Log.Warning("Failed to find orb effect index");
+            }
+        }
+
+        public float SteakBonus;
+
+        CharacterMasterExtraStatsTracker _targetMasterStats;
+
+        public override void Begin()
+        {
+            duration = Mathf.Max(Time.fixedDeltaTime, distanceToTarget / 30f);
+
+            if (_orbEffectIndex != EffectIndex.Invalid)
+            {
+                EffectData effectData = new EffectData
+                {
+                    origin = origin,
+                    genericFloat = duration
+                };
+                
+                effectData.SetHurtBoxReference(target);
+
+                EffectManager.SpawnEffect(_orbEffectIndex, effectData, true);
+            }
+
+            HealthComponent targetHealthComponent = target ? target.healthComponent : null;
+            CharacterBody targetBody = targetHealthComponent ? targetHealthComponent.body : null;
+            CharacterMaster targetMaster = targetBody ? targetBody.master : null;
+            if (targetMaster)
+            {
+                _targetMasterStats = targetMaster.GetComponent<CharacterMasterExtraStatsTracker>();
+            }
+        }
+
+        public override void OnArrival()
+        {
+            if (_targetMasterStats)
+            {
+                _targetMasterStats.SteakBonus += SteakBonus;
+            }
+        }
+    }
+}
