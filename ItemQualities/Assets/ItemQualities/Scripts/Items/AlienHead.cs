3616406e7efd06573a77aa3ba41ec5acf3150f0c
Goorakh <petter.ohlen@gmail.com>
2025-10-11 02:41:17 +0200

    Add Alien Head qualities (#4)

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/AlienHead.cs
@@ -0,0 +1,39 @@
+﻿using ItemQualities.Utilities;
+using RoR2;
+using UnityEngine.Networking;
+
+namespace ItemQualities.Items
+{
+    static class AlienHead
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            GlobalEventManager.onCharacterDeathGlobal += onCharacterDeathGlobal;
+        }
+
+        static void onCharacterDeathGlobal(DamageReport deathReport)
+        {
+            if (!NetworkServer.active)
+                return;
+
+            if (!deathReport?.attackerBody)
+                return;
+
+            ItemQualityCounts alienHead = ItemQualitiesContent.ItemQualityGroups.AlienHead.GetItemCounts(deathReport.attackerBody.inventory);
+            if (alienHead.TotalQualityCount > 0)
+            {
+                float cooldownReductionChance = (15f * alienHead.UncommonCount) +
+                                                (35f * alienHead.RareCount) +
+                                                (75f * alienHead.EpicCount) +
+                                                (100f * alienHead.LegendaryCount);
+
+                float cooldownReduction = 1f * RollUtil.GetOverflowRoll(cooldownReductionChance, deathReport.attackerMaster);
+                if (cooldownReduction > 0f)
+                {
+                    deathReport.attackerBody.skillLocator.DeductCooldownFromAllSkillsServer(cooldownReduction);
+                }
+            }
+        }
+    }
+}
---
+++ ItemQualities/Assets/ItemQualities/Scripts/Utilities/RollUtil.cs
@@ -0,0 +1,19 @@
+﻿using RoR2;
+
+namespace ItemQualities.Utilities
+{
+    public static class RollUtil
+    {
+        public static int GetOverflowRoll(float percentChance, CharacterMaster master)
+        {
+            int roll = (int)(percentChance / 100f);
+
+            if (Util.CheckRoll(percentChance % 100f, master))
+            {
+                roll++;
+            }
+
+            return roll;
+        }
+    }
+}
