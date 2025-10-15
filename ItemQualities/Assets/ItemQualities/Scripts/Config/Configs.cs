513436372133f53ead3a7406708be9794da38ba6
Gorakh <petter.ohlen@gmail.com>
2025-10-15 19:23:14 +0200

    Add item qualities log config

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Config/Configs.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx.Configuration;
+using RiskOfOptions;
+
+namespace ItemQualities
+{
+    public static partial class Configs
+    {
+        const string ModGuid = ItemQualitiesPlugin.PluginGUID;
+        const string ModName = ItemQualitiesPlugin.PluginName;
+
+        internal static void Init(ConfigFile configFile)
+        {
+#if DEBUG
+            Debug.Init(configFile);
+#endif
+        }
+
+        internal static void InitRiskOfOptions()
+        {
+            ModSettingsManager.SetModDescription("Settings for ItemQualities", ModGuid, ModName);
+
+            // TODO
+            //ModSettingsManager.SetModIcon()
+
+#if DEBUG
+            Debug.InitRiskOfOptions();
+#endif
+        }
+    }
+}
