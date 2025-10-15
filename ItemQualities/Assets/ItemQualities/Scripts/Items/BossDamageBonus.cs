978744bbc0b941280589c8b9da9c518357de943d
Gorakh <petter.ohlen@gmail.com>
2025-10-15 18:12:51 +0200

    Add Armor Piercing Rounds qualities

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/BossDamageBonus.cs
@@ -0,0 +1,107 @@
+﻿using HG;
+using ItemQualities.Utilities.Extensions;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine.Networking;
+
+namespace ItemQualities.Items
+{
+    static class BossDamageBonus
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            TeleporterInteraction.onTeleporterBeginChargingGlobal += onTeleporterBeginChargingGlobal;
+
+            IL.RoR2.HealthComponent.TakeDamageProcess += HealthComponent_TakeDamageProcess;
+        }
+
+        static void onTeleporterBeginChargingGlobal(TeleporterInteraction teleporterInteraction)
+        {
+            if (NetworkServer.active)
+            {
+                teleporterInteraction.gameObject.EnsureComponent<BossDamageBonusTeleporterController>();
+            }
+        }
+
+        static void HealthComponent_TakeDamageProcess(ILContext il)
+        {
+            if (!il.Method.TryFindParameter<DamageInfo>(out ParameterDefinition damageInfoParameter))
+            {
+                Log.Error("Failed to find DamageInfo parameter");
+                return;
+            }
+
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryFindNext(out ILCursor[] foundCursors,
+                               x => x.MatchLdsfld(typeof(RoR2Content.Items), nameof(RoR2Content.Items.BossDamageBonus)),
+                               x => x.MatchLdcR4(0.2f),
+                               x => x.MatchMul()))
+            {
+                Log.Error("Failed to find damage patch location");
+                return;
+            }
+
+            c.Goto(foundCursors[0].Next, MoveType.Before); // ldsfld RoR2Content.Items.BossDamageBonus
+            if (!c.TryGotoPrev(MoveType.After,
+                               x => x.MatchCallOrCallvirt<CharacterBody>("get_" + nameof(CharacterBody.isBoss))))
+            {
+                Log.Error("Failed to find isBoss patch location");
+                return;
+            }
+
+            VariableDefinition isMiniBossVar = il.AddVariable<bool>();
+
+            c.Emit(OpCodes.Dup);
+            c.Emit(OpCodes.Ldarg_0);
+            c.Emit(OpCodes.Ldarg, damageInfoParameter);
+            c.EmitDelegate<Func<bool, HealthComponent, DamageInfo, bool>>(isMiniBoss);
+            c.Emit(OpCodes.Dup);
+            c.Emit(OpCodes.Stloc, isMiniBossVar);
+            c.Emit(OpCodes.Or);
+
+            static bool isMiniBoss(bool isBoss, HealthComponent victim, DamageInfo damageInfo)
+            {
+                if (isBoss)
+                    return false;
+
+                if (!victim || !victim.body)
+                    return false;
+
+                CharacterBody attackerBody = damageInfo?.attacker ? damageInfo.attacker.GetComponent<CharacterBody>() : null;
+                Inventory attackerInventory = attackerBody ? attackerBody.inventory : null;
+
+                ItemQualityCounts bossDamageBonus = ItemQualitiesContent.ItemQualityGroups.BossDamageBonus.GetItemCounts(attackerInventory);
+                return bossDamageBonus.TotalQualityCount > 0 && victim.body.HasBuff(ItemQualitiesContent.Buffs.MiniBossMarker);
+            }
+
+            c.Goto(foundCursors[2].Next, MoveType.After); // mul
+
+            c.Emit(OpCodes.Ldloc, isMiniBossVar);
+            c.Emit(OpCodes.Ldarg, damageInfoParameter);
+            c.EmitDelegate<Func<float, bool, DamageInfo, float>>(getBossDamageMultiplier);
+
+            static float getBossDamageMultiplier(float damageMultiplier, bool isMiniBoss, DamageInfo damageInfo)
+            {
+                if (isMiniBoss)
+                {
+                    CharacterBody attackerBody = damageInfo?.attacker ? damageInfo.attacker.GetComponent<CharacterBody>() : null;
+                    Inventory attackerInventory = attackerBody ? attackerBody.inventory : null;
+
+                    ItemQualityCounts bossDamageBonus = ItemQualitiesContent.ItemQualityGroups.BossDamageBonus.GetItemCounts(attackerInventory);
+
+                    damageMultiplier = (0.05f * bossDamageBonus.UncommonCount) +
+                                       (0.10f * bossDamageBonus.RareCount) +
+                                       (0.15f * bossDamageBonus.EpicCount) +
+                                       (0.20f * bossDamageBonus.LegendaryCount);
+                }
+
+                return damageMultiplier;
+            }
+        }
+    }
+}
---
+++ ItemQualities/Assets/ItemQualities/Scripts/PositionIndicatorAttachment.cs
@@ -0,0 +1,19 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace ItemQualities
+{
+    [RequireComponent(typeof(NetworkedBodyAttachment))]
+    public class PositionIndicatorAttachment : MonoBehaviour, INetworkedBodyAttachmentListener
+    {
+        public PositionIndicator PositionIndicator;
+
+        void INetworkedBodyAttachmentListener.OnAttachedBodyDiscovered(NetworkedBodyAttachment networkedBodyAttachment, CharacterBody attachedBody)
+        {
+            if (PositionIndicator)
+            {
+                PositionIndicator.targetTransform = attachedBody.coreTransform;
+            }
+        }
+    }
+}
