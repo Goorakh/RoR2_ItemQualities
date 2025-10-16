a0ee341545208ffdbdfedff2eb769abe6747bfcd
Gorakh <petter.ohlen@gmail.com>
2025-10-16 12:10:50 +0200

    "Fix" large quality chest spawning(?)

--- ItemQualities/Assets/ItemQualities/Scripts/CustomInteractableHandler.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/CustomInteractableHandler.cs
@@ -34,7 +34,7 @@ namespace ItemQualities
                 InteractableCategorySelectionWeight = QualityCategoryWeight,
                 CustomInteractableCategory = QualityCategoryName,
                 InteractableCategory = DirectorAPI.InteractableCategory.Custom,
-            }, containsAnyLargeCategoryChest);
+            }, containsAnyCategoryChest);
 
             DirectorAPI.Helpers.AddNewInteractable(new DirectorAPI.DirectorCardHolder
             {
@@ -46,7 +46,7 @@ namespace ItemQualities
                 InteractableCategorySelectionWeight = QualityCategoryWeight,
                 CustomInteractableCategory = QualityCategoryName,
                 InteractableCategory = DirectorAPI.InteractableCategory.Custom,
-            }, containsAnySmallCategoryChest);
+            }, containsAnyCategoryChest);
         }
 
         static bool matchDirectorCard(DirectorCard directorCard, string spawnCardName, string spawnCardGuid)
@@ -74,33 +74,18 @@ namespace ItemQualities
             return false;
         }
 
-        static bool containsAnySmallCategoryChest(DirectorCardCategorySelection dccs)
+        static bool containsAnyCategoryChest(DirectorCardCategorySelection dccs)
         {
             foreach (DirectorCardCategorySelection.Category category in dccs.categories)
             {
                 foreach (DirectorCard card in category.cards)
                 {
                     if (matchDirectorCard(card, "iscCategoryChestDamage", RoR2_Base_CategoryChest.iscCategoryChestDamage_asset) ||
+                        matchDirectorCard(card, "iscCategoryChest2Damage", RoR2_DLC1_CategoryChest2.iscCategoryChest2Damage_asset) ||
+                        matchDirectorCard(card, "iscCategoryChestUtility", RoR2_Base_CategoryChest.iscCategoryChestUtility_asset) ||
+                        matchDirectorCard(card, "iscCategoryChest2Utility", RoR2_DLC1_CategoryChest2.iscCategoryChest2Utility_asset) ||
                         matchDirectorCard(card, "iscCategoryChestHealing", RoR2_Base_CategoryChest.iscCategoryChestHealing_asset) ||
-                        matchDirectorCard(card, "iscCategoryChestUtility", RoR2_Base_CategoryChest.iscCategoryChestUtility_asset))
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        static bool containsAnyLargeCategoryChest(DirectorCardCategorySelection dccs)
-        {
-            foreach (DirectorCardCategorySelection.Category category in dccs.categories)
-            {
-                foreach (DirectorCard card in category.cards)
-                {
-                    if (matchDirectorCard(card, "iscCategoryChest2Damage", RoR2_DLC1_CategoryChest2.iscCategoryChest2Damage_asset) ||
-                        matchDirectorCard(card, "iscCategoryChest2Healing", RoR2_DLC1_CategoryChest2.iscCategoryChest2Healing_asset) ||
-                        matchDirectorCard(card, "iscCategoryChest2Utility", RoR2_DLC1_CategoryChest2.iscCategoryChest2Utility_asset))
+                        matchDirectorCard(card, "iscCategoryChest2Healing", RoR2_DLC1_CategoryChest2.iscCategoryChest2Healing_asset))
                     {
                         return true;
                     }
