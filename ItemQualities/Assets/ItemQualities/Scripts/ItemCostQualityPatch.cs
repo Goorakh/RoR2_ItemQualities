9fd933c6d1399630ce787218e3dd6ad4cfb53f02
Gorakh <petter.ohlen@gmail.com>
2025-10-24 14:40:53 +0200

    Disable using quality items on item cost interactables

--- ItemQualities/Assets/ItemQualities/Scripts/ItemCostQualityPatch.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/ItemCostQualityPatch.cs
@@ -1,19 +1,100 @@
-﻿using Mono.Cecil.Cil;
+﻿using ItemQualities.Utilities.Extensions;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using RoR2;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace ItemQualities
 {
     static class ItemCostQualityPatch
     {
-        [SystemInitializer]
+        [SystemInitializer(typeof(CostTypeCatalog))]
         static void Init()
         {
+            CostTypeDef whiteItemCostDef = CostTypeCatalog.GetCostTypeDef(CostTypeIndex.WhiteItem);
+            if (whiteItemCostDef?.isAffordable?.Method != null)
+            {
+                new Hook(whiteItemCostDef.isAffordable.Method, new Func<CostTypeCatalog_IsAffordableItem_orig, CostTypeDef, CostTypeDef.IsAffordableContext, bool>(CostTypeCatalog_IsAffordableItem));
+            }
+            else
+            {
+                Log.Error("Failed to find IsAffordableItem method");
+            }
+
+            if (whiteItemCostDef?.payCost?.Method != null)
+            {
+                new ILHook(whiteItemCostDef.payCost.Method, CostTypeCatalog_PayCostItems);
+            }
+            else
+            {
+                Log.Error("Failed to find PayCostItems method");
+            }
+
             IL.RoR2.ChestBehavior.BaseItemDrop += ChestBehavior_BaseItemDrop;
             IL.RoR2.ShopTerminalBehavior.DropPickup += ShopTerminalBehavior_DropPickup;
+        }
+
+        delegate bool CostTypeCatalog_IsAffordableItem_orig(CostTypeDef costTypeDef, CostTypeDef.IsAffordableContext context);
+        static bool CostTypeCatalog_IsAffordableItem(CostTypeCatalog_IsAffordableItem_orig orig, CostTypeDef costTypeDef, CostTypeDef.IsAffordableContext context)
+        {
+            if (!orig(costTypeDef, context))
+                return false;
+
+            bool hasEnoughNonQualityItems = false;
+            if (context.activator && context.activator.TryGetComponent(out CharacterBody activatorBody) && activatorBody.inventory)
+            {
+                int foundItems = 0;
+                foreach (ItemDef itemDef in ItemCatalog.allItemDefs)
+                {
+                    if (itemDef && itemDef.tier == costTypeDef.itemTier && QualityCatalog.GetQualityTier(itemDef.itemIndex) == QualityTier.None)
+                    {
+                        foundItems += activatorBody.inventory.GetItemCount(itemDef);
+                        if (foundItems >= context.cost)
+                        {
+                            hasEnoughNonQualityItems = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return hasEnoughNonQualityItems;
+        }
+
+        static void CostTypeCatalog_PayCostItems(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            ILLabel skipItemLabel = default;
+            if (!c.TryFindNext(out ILCursor[] foundCursors,
+                               x => x.MatchLdfld<CostTypeDef.PayCostContext>(nameof(CostTypeDef.PayCostContext.avoidedItemIndex)),
+                               x => x.MatchBeq(out skipItemLabel)))
+            {
+                Log.Error("Failed to find patch location");
+                return;
+            }
+
+            c.Goto(foundCursors[1].Next, MoveType.After); // beq
+
+            if (!c.TryFindForeachVariable(out VariableDefinition itemIndexVar))
+            {
+                Log.Error("Failed to find ItemIndex variable");
+                return;
+            }
+
+            c.Emit(OpCodes.Ldloc, itemIndexVar);
+            c.EmitDelegate<Func<ItemIndex, bool>>(isQualityItem);
+            c.Emit(OpCodes.Brtrue, skipItemLabel);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static bool isQualityItem(ItemIndex itemIndex)
+            {
+                return QualityCatalog.GetQualityTier(itemIndex) > QualityTier.None;
+            }
         }
 
         static PickupIndex tryUpgradeQualityFromCost(PickupIndex intendedDropPickupIndex, GameObject dropperObject, Xoroshiro128Plus rng)
