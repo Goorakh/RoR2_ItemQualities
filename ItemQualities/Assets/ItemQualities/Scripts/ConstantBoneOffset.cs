28c0002006a28b6aa632d3062d421d765a36459c
Gorakh <petter.ohlen@gmail.com>
2025-10-26 17:23:09 +0100

    Temporary fix for small quality chest lid offset

---
+++ ItemQualities/Assets/ItemQualities/Scripts/ConstantBoneOffset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ItemQualities
+{
+    public class ConstantBoneOffset : MonoBehaviour
+    {
+        public Vector3 PositionOffset;
+
+        [SerializeField]
+        Animator _animator;
+
+        Vector3 _lastLocalPosition;
+
+        void OnEnable()
+        {
+            _lastLocalPosition = Vector3.positiveInfinity;
+        }
+
+        void LateUpdate()
+        {
+            if (_animator && _animator.isActiveAndEnabled)
+            {
+                if ((transform.localPosition - _lastLocalPosition).sqrMagnitude >= 0.01f * 0.01f)
+                {
+                    transform.localPosition += PositionOffset;
+                    _lastLocalPosition = transform.localPosition;
+                }
+            }
+        }
+    }
+}
