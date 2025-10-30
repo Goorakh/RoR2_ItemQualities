b85254578956494363e7b337ba8619b8f3323338
Gorakh <petter.ohlen@gmail.com>
2025-10-30 07:56:07 +0100

    Experiment: Increase difficulty scaling

--- ItemQualities/Assets/ItemQualities/Scripts/Config/General.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Config/General.cs
@@ -2,6 +2,7 @@
 using RiskOfOptions;
 using RiskOfOptions.OptionConfigs;
 using RiskOfOptions.Options;
+using System.Runtime.CompilerServices;
 
 namespace ItemQualities
 {
@@ -11,15 +12,22 @@ namespace ItemQualities
         {
             const string SectionName = "General";
 
+            public static ConfigEntry<bool> EnableDifficultyModifications { get; private set; }
+
             public static ConfigEntry<float> GlobalQualityChance { get; private set; }
 
             internal static void Init(ConfigFile configFile)
             {
+                EnableDifficultyModifications = configFile.Bind(new ConfigDefinition(SectionName, "Difficulty Changes"), true, new ConfigDescription("Enable balance difficulty changes"));
+
                 GlobalQualityChance = configFile.Bind(new ConfigDefinition(SectionName, "Global Quality Chance"), 4f, new ConfigDescription("The % chance for an item not from a quality chest to be of quality", new AcceptableValueRange<float>(0f, 100f)));
             }
 
+            [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
             internal static void InitRiskOfOptions()
             {
+                ModSettingsManager.AddOption(new CheckBoxOption(EnableDifficultyModifications));
+
                 ModSettingsManager.AddOption(new SliderOption(GlobalQualityChance, new SliderConfig
                 {
                     min = 0f,
---
+++ ItemQualities/Assets/ItemQualities/Scripts/DifficultyScaling.cs
@@ -0,0 +1,50 @@
+﻿using MonoMod.Cil;
+using RoR2;
+using System;
+
+namespace ItemQualities
+{
+    static class DifficultyScaling
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            IL.RoR2.Run.RecalculateDifficultyCoefficentInternal += ApplyScalingValueChangesPatch;
+            IL.RoR2.InfiniteTowerRun.RecalculateDifficultyCoefficentInternal += ApplyScalingValueChangesPatch;
+        }
+
+        static void ApplyScalingValueChangesPatch(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            int patchCount = 0;
+
+            while (c.TryGotoNext(MoveType.After,
+                                 x => x.MatchLdfld<DifficultyDef>(nameof(DifficultyDef.scalingValue))))
+            {
+                c.EmitDelegate<Func<float, float>>(getScalingValue);
+
+                static float getScalingValue(float scalingValue)
+                {
+                    if (Configs.General.EnableDifficultyModifications.Value)
+                    {
+                        scalingValue *= 1.4f;
+                    }
+
+                    return scalingValue;
+                }
+
+                patchCount++;
+            }
+
+            if (patchCount == 0)
+            {
+                Log.Error($"Failed to find patch location for {il.Method.FullName}");
+            }
+            else
+            {
+                Log.Debug($"Found {patchCount} patch location(s) for {il.Method.FullName}");
+            }
+        }
+    }
+}
