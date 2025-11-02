2b2d8f087a55e44784d56a960582bf82aff5d337
Gorakh <petter.ohlen@gmail.com>
2025-11-02 17:42:49 +0100

    Start of Monster Tooth rework

--- ItemQualities/Assets/ItemQualities/Scripts/Items/Tooth.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/Tooth.cs
@@ -5,7 +5,6 @@ using MonoMod.Cil;
 using RoR2;
 using System;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace ItemQualities.Items
 {
@@ -29,75 +28,145 @@ namespace ItemQualities.Items
 
             if (!c.TryFindNext(out ILCursor[] foundCursors,
                                x => x.MatchLdsfld(typeof(RoR2Content.Items), nameof(RoR2Content.Items.Tooth)),
-                               x => x.MatchCallOrCallvirt(typeof(NetworkServer), nameof(NetworkServer.Spawn))))
+                               x => x.MatchCallOrCallvirt(typeof(LegacyResourcesAPI), nameof(LegacyResourcesAPI.Load)),
+                               x => x.MatchCallOrCallvirt(typeof(UnityEngine.Object), nameof(UnityEngine.Object.Instantiate))))
             {
                 Log.Error("Failed to find patch location");
                 return;
             }
 
-            c.Goto(foundCursors[1].Next, MoveType.Before);
-            c.Emit(OpCodes.Dup);
+            c.Goto(foundCursors[1].Next, MoveType.After);
+
+            VariableDefinition orbParametersVar = il.AddVariable<ToothOrbParameters>();
+
             c.Emit(OpCodes.Ldarg, damageReportParameter);
-            c.EmitDelegate<Action<GameObject, DamageReport>>(beforeHealingOrbSpawn);
+            c.Emit(OpCodes.Ldloca, orbParametersVar);
+            c.EmitDelegate<GetHealingOrbPrefabDelegate>(getHealingOrbPrefab);
 
-            static void beforeHealingOrbSpawn(GameObject healingOrb, DamageReport damageReport)
+            static GameObject getHealingOrbPrefab(GameObject healingOrbPrefab, DamageReport damageReport, out ToothOrbParameters orbParameters)
             {
-                if (!healingOrb || damageReport == null)
-                    return;
+                float flatOrbValue = 0f;
+                float fractionalOrbValue = 0f;
+                BuffDef orbBuff = null;
 
-                HealthPickup healthPickup = healingOrb.GetComponentInChildren<HealthPickup>();
-                GravitatePickup gravitatePickup = healingOrb.GetComponentInChildren<GravitatePickup>();
-                SphereCollider gravitatePickupTrigger = gravitatePickup ? gravitatePickup.GetComponent<SphereCollider>() : null;
-
-                CharacterMaster attackerMaster = damageReport.attackerMaster;
-                Inventory attackerInventory = attackerMaster ? attackerMaster.inventory : null;
-
-                ItemQualityCounts tooth = default;
-                if (attackerInventory)
+                if (damageReport?.damageInfo != null)
                 {
-                    tooth = ItemQualitiesContent.ItemQualityGroups.Tooth.GetItemCounts(attackerInventory);
+                    CharacterMaster attackerMaster = damageReport.attackerMaster;
+                    Inventory attackerInventory = attackerMaster ? attackerMaster.inventory : null;
+
+                    ItemQualityCounts tooth = ItemQualitiesContent.ItemQualityGroups.Tooth.GetItemCounts(attackerInventory);
+                    if (tooth.TotalQualityCount > 0)
+                    {
+                        GameObject overrideHealOrbPrefab = null;
+
+                        DamageSource damageSource = damageReport.damageInfo.damageType.damageSource;
+                        if ((damageSource & DamageSource.Primary) != 0)
+                        {
+                            overrideHealOrbPrefab = ItemQualitiesContent.NetworkedPrefabs.HealOrbPrimary;
+
+                            float buffDuration = (1f * tooth.UncommonCount) +
+                                                 (2f * tooth.RareCount) +
+                                                 (3f * tooth.EpicCount) +
+                                                 (5f * tooth.LegendaryCount);
+
+                            flatOrbValue = buffDuration;
+
+                            orbBuff = BuffCatalog.GetBuffDef(ItemQualitiesContent.BuffQualityGroups.ToothPrimaryBuff.GetBuffIndex(tooth.HighestQuality));
+                        }
+                        else if ((damageSource & DamageSource.Secondary) != 0)
+                        {
+                            overrideHealOrbPrefab = ItemQualitiesContent.NetworkedPrefabs.HealOrbSecondary;
+
+                            float buffDuration = (2f * tooth.UncommonCount) +
+                                                 (4f * tooth.RareCount) +
+                                                 (6f * tooth.EpicCount) +
+                                                 (10f * tooth.LegendaryCount);
+
+                            flatOrbValue = buffDuration;
+
+                            orbBuff = BuffCatalog.GetBuffDef(ItemQualitiesContent.BuffQualityGroups.ToothSecondaryBuff.GetBuffIndex(tooth.HighestQuality));
+                        }
+                        else if ((damageSource & DamageSource.Utility) != 0)
+                        {
+                            overrideHealOrbPrefab = ItemQualitiesContent.NetworkedPrefabs.HealOrbUtility;
+
+                            float flatBarrier = (15f * tooth.UncommonCount) +
+                                                (25f * tooth.RareCount) +
+                                                (35f * tooth.EpicCount) +
+                                                (50f * tooth.LegendaryCount);
+
+                            flatOrbValue = flatBarrier;
+                        }
+                        else if ((damageSource & DamageSource.Special) != 0)
+                        {
+                            overrideHealOrbPrefab = ItemQualitiesContent.NetworkedPrefabs.HealOrbSpecial;
+
+                            float flatCooldownReduction = (1f * tooth.UncommonCount) +
+                                                          (2f * tooth.RareCount) +
+                                                          (3f * tooth.EpicCount) +
+                                                          (5f * tooth.LegendaryCount);
+
+                            flatOrbValue = flatCooldownReduction;
+                        }
+
+                        if (overrideHealOrbPrefab)
+                        {
+                            healingOrbPrefab = overrideHealOrbPrefab;
+                        }
+                    }
                 }
 
-                float healingMultiplier = 1f;
-                healingMultiplier += 0.2f * tooth.UncommonCount;
-                healingMultiplier += 0.4f * tooth.RareCount;
-                healingMultiplier += 0.7f * tooth.EpicCount;
-                healingMultiplier += 1.0f * tooth.LegendaryCount;
+                orbParameters = new ToothOrbParameters(flatOrbValue, fractionalOrbValue, orbBuff);
+                return healingOrbPrefab;
+            }
 
-                float pickupRangeMultiplier = 1f;
-                pickupRangeMultiplier += 0.2f * tooth.UncommonCount;
-                pickupRangeMultiplier += 0.4f * tooth.RareCount;
-                pickupRangeMultiplier += 0.7f * tooth.EpicCount;
-                pickupRangeMultiplier += 1.0f * tooth.LegendaryCount;
+            c.Goto(foundCursors[2].Next, MoveType.After); // call UnityEngine.Object.Instantiate
 
-                float bigOrbChance = (5f * tooth.UncommonCount) +
-                                     (15f * tooth.RareCount) +
-                                     (25f * tooth.EpicCount) +
-                                     (50f * tooth.LegendaryCount);
+            c.Emit(OpCodes.Dup);
+            c.Emit(OpCodes.Ldloc, orbParametersVar);
+            c.EmitDelegate<Action<GameObject, ToothOrbParameters>>(setupHealingOrb);
 
-                if (Util.CheckRoll(bigOrbChance, attackerMaster))
+            static void setupHealingOrb(GameObject healingOrb, ToothOrbParameters orbParameters)
+            {
+                if (healingOrb && (orbParameters.FlatValue > 0f || orbParameters.FractionalValue > 0f))
                 {
-                    healingMultiplier *= 2f;
-                    pickupRangeMultiplier *= 1.5f;
-                }
+                    BuffPickup buffPickup = healingOrb.GetComponentInChildren<BuffPickup>();
+                    if (buffPickup)
+                    {
+                        buffPickup.buffDuration = orbParameters.FlatValue;
+                        buffPickup.buffDef = orbParameters.Buff;
+                    }
 
-                if (healthPickup)
-                {
-                    healthPickup.fractionalHealing *= healingMultiplier;
-                }
+                    BarrierPickup barrierPickup = healingOrb.GetComponentInChildren<BarrierPickup>();
+                    if (barrierPickup)
+                    {
+                        barrierPickup.FlatAmount = orbParameters.FlatValue;
+                        barrierPickup.FractionalAmount = orbParameters.FractionalValue;
+                    }
 
-                if (healingMultiplier > 1f)
-                {
-                    healingOrb.transform.localScale *= Mathf.Pow(healingMultiplier, 0.5f);
+                    SkillCooldownPickup skillCooldownPickup = healingOrb.GetComponentInChildren<SkillCooldownPickup>();
+                    if (skillCooldownPickup)
+                    {
+                        skillCooldownPickup.FlatAmount = orbParameters.FlatValue;
+                        skillCooldownPickup.FractionalAmount = orbParameters.FractionalValue;
+                    }
                 }
+            }
+        }
 
-                if (gravitatePickupTrigger)
-                {
-                    // This probably doesn't need to be networked..?
-                    // Even though the gravitate trigger is *technically* simulated clientside,
-                    // the transform networker overrides the position so it probably doesn't matter in the end?
-                    gravitatePickupTrigger.radius *= pickupRangeMultiplier;
-                }
+        delegate GameObject GetHealingOrbPrefabDelegate(GameObject healingOrbPrefab, DamageReport damageReport, out ToothOrbParameters orbParameters);
+
+        readonly struct ToothOrbParameters
+        {
+            public readonly float FlatValue;
+            public readonly float FractionalValue;
+            public readonly BuffDef Buff;
+
+            public ToothOrbParameters(float flatValue, float fractionalValue, BuffDef buff)
+            {
+                FlatValue = flatValue;
+                FractionalValue = fractionalValue;
+                Buff = buff;
             }
         }
     }
