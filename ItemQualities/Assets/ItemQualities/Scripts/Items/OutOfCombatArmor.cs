96a0da9e287760d19e5e1d231d9957241b75f309
Onyx <50289874+Tizitendo@users.noreply.github.com>
2025-11-09 22:53:07 +0100

    Opal rework (#69)

--- ItemQualities/Assets/ItemQualities/Scripts/Items/OutOfCombatArmor.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/OutOfCombatArmor.cs
@@ -1,21 +1,56 @@
-﻿using ItemQualities.Utilities.Extensions;
+﻿using HG.Coroutines;
+using ItemQualities.ContentManagement;
+using ItemQualities.Utilities;
+using ItemQualities.Utilities.Extensions;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using R2API;
 using RoR2;
+using RoR2BepInExPack.GameAssetPathsBetter;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace ItemQualities.Items
 {
     static class OutOfCombatArmor
     {
-        [SystemInitializer]
+		static readonly SphereSearch _opalSphereSearch = new SphereSearch();
+		static readonly List<HurtBox> _opalHurtBoxBuffer = new List<HurtBox>();
+		static GameObject _explosionVFX;
+
+		[SystemInitializer]
         static void Init()
         {
             IL.RoR2.CharacterBody.OnTakeDamageServer += CharacterBody_OnTakeDamageServer;
         }
 
-        static void CharacterBody_OnTakeDamageServer(ILContext il)
+		[ContentInitializer]
+		static IEnumerator LoadContent(ContentIntializerArgs args)
+		{
+			AsyncOperationHandle<GameObject> igniteOnKillExplosionLoad = AddressableUtil.LoadTempAssetAsync<GameObject>(RoR2_Base_IgniteOnKill.IgniteExplosionVFX_prefab);
+
+			ParallelProgressCoroutine prefabsLoadCoroutine = new ParallelProgressCoroutine(args.ProgressReceiver);
+			prefabsLoadCoroutine.Add(igniteOnKillExplosionLoad);
+
+			yield return prefabsLoadCoroutine;
+
+			if (igniteOnKillExplosionLoad.Status != AsyncOperationStatus.Succeeded || !igniteOnKillExplosionLoad.Result)
+			{
+				Log.Error($"Failed to load igniteOnKill VFX prefab: {igniteOnKillExplosionLoad.OperationException}");
+				yield break;
+			}
+
+			_explosionVFX = igniteOnKillExplosionLoad.Result.InstantiateClone("opalExplosionVFX", false);
+			UnityEngine.Object.Destroy(_explosionVFX.transform.Find("Flames").gameObject);
+			UnityEngine.Object.Destroy(_explosionVFX.transform.Find("Flash").gameObject);
+			args.ContentPack.effectDefs.Add(new EffectDef(_explosionVFX));
+		}
+
+		static void CharacterBody_OnTakeDamageServer(ILContext il)
         {
             if (!il.Method.TryFindParameter<DamageReport>(out ParameterDefinition damageReportParameter))
             {
@@ -38,28 +73,64 @@ namespace ItemQualities.Items
 
             static void onEnterDanger(CharacterBody victim, DamageReport damageReport)
             {
-                if (!victim || damageReport?.damageInfo == null)
-                    return;
+                if (!victim || damageReport?.damageInfo == null) return;
+				CharacterBody body = victim.GetBody();
+				if (!body) return;
 
-                if (!damageReport.attacker)
-                    return;
+				float radius = 0;
+				switch (ItemQualitiesContent.ItemQualityGroups.OutOfCombatArmor.GetHighestQualityInInventory(body.master.inventory))
+				{
+					case QualityTier.Uncommon:
+						radius = 10;
+						break;
+					case QualityTier.Rare:
+						radius = 20;
+						break;
+					case QualityTier.Epic:
+						radius = 30;
+						break;
+					case QualityTier.Legendary:
+						radius = 50;
+						break;
+					default:
+						return;
+				}
+
+				ItemQualityCounts outOfCombatArmor = ItemQualitiesContent.ItemQualityGroups.OutOfCombatArmor.GetItemCounts(body.master.inventory);
+				float duration =	outOfCombatArmor.UncommonCount * 2 +
+									outOfCombatArmor.RareCount * 4 +
+									outOfCombatArmor.EpicCount * 6 +
+									outOfCombatArmor.LegendaryCount * 8;
 
                 if (victim.HasBuff(DLC1Content.Buffs.OutOfCombatArmorBuff))
                 {
-                    ItemQualityCounts outOfCombatArmor = ItemQualitiesContent.ItemQualityGroups.OutOfCombatArmor.GetItemCounts(victim.inventory);
-                    if (outOfCombatArmor.TotalQualityCount > 0)
-                    {
-                        float stunDuration = (1f * outOfCombatArmor.UncommonCount) +
-                                             (2f * outOfCombatArmor.RareCount) +
-                                             (4f * outOfCombatArmor.EpicCount) +
-                                             (6f * outOfCombatArmor.LegendaryCount);
+					_opalSphereSearch.origin = body.corePosition;
+					_opalSphereSearch.mask = LayerIndex.entityPrecise.mask;
+					_opalSphereSearch.radius = radius;
+					_opalSphereSearch.RefreshCandidates();
+					_opalSphereSearch.FilterCandidatesByHurtBoxTeam(TeamMask.GetUnprotectedTeams(body.master.teamIndex));
+					_opalSphereSearch.FilterCandidatesByDistinctHurtBoxEntities();
+					_opalSphereSearch.GetHurtBoxes(_opalHurtBoxBuffer);
+					_opalSphereSearch.ClearCandidates();
+					for (int i = 0; i < _opalHurtBoxBuffer.Count; i++)
+					{
+						HurtBox hurtBox = _opalHurtBoxBuffer[i];
+						if (hurtBox.healthComponent && hurtBox.healthComponent.body != body)
+						{
+							if (hurtBox.healthComponent.TryGetComponent(out SetStateOnHurt attackerSetStateOnHurt) && attackerSetStateOnHurt.canBeStunned) {
+								attackerSetStateOnHurt.SetStun(duration);
+							}
+						}
+					}
+					_opalHurtBoxBuffer.Clear();
 
-                        if (damageReport.attacker.TryGetComponent(out SetStateOnHurt attackerSetStateOnHurt) && attackerSetStateOnHurt.canBeStunned)
-                        {
-                            attackerSetStateOnHurt.SetStun(stunDuration);
-                        }
-                    }
-                }
+					EffectManager.SpawnEffect(_explosionVFX, new EffectData
+					{
+						origin = victim.corePosition,
+						scale = radius,
+						rotation = Util.QuaternionSafeLookRotation(damageReport.damageInfo.force)
+					}, transmit: true);
+				}
             }
         }
     }
