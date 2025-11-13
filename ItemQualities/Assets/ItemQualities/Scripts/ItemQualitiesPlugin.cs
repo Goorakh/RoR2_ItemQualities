3cb78ea67a9eac355d4a8cd6f66fe8d0bbef7932
Gorakh <petter.ohlen@gmail.com>
2025-11-13 17:27:33 +0100

    Bump version

--- ItemQualities/Assets/ItemQualities/Scripts/ItemQualitiesPlugin.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/ItemQualitiesPlugin.cs
@@ -27,7 +27,7 @@ namespace ItemQualities
         public const string PluginName = "ItemQualities";
         public const string PluginAuthor = "Gorakh";
         public const string PluginGUID = "com." + PluginAuthor + "." + PluginName;
-        public const string PluginVersion = "0.2.0";
+        public const string PluginVersion = "0.3.0";
 
         static ItemQualitiesPlugin _instance;
         public static ItemQualitiesPlugin Instance => _instance;
