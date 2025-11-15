3048eb7936fbd6a44722b9e244efca38b6083e78
Gorakh <petter.ohlen@gmail.com>
2025-11-15 20:01:29 +0100

    PSG tweaks/fixes
    * Add lighter shield bar for temp shield
    * Increase numbers a bit
    * Fix non-valid interactables giving temp shield

--- ItemQualities/Assets/ItemQualities/Scripts/Items/PersonalShield.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/PersonalShield.cs
@@ -10,50 +10,67 @@ namespace ItemQualities.Items
         [SystemInitializer]
         static void Init()
         {
-			RecalculateStatsAPI.GetStatCoefficients += getStatCoefficients;
-			GlobalEventManager.OnInteractionsGlobal += OnInteractionsGlobal;
-			GlobalEventManager.onServerDamageDealt += onServerDamageDealt;
-		}
+            RecalculateStatsAPI.GetStatCoefficients += getStatCoefficients;
+            GlobalEventManager.OnInteractionsGlobal += OnInteractionsGlobal;
+            GlobalEventManager.onServerDamageDealt += onServerDamageDealt;
+        }
 
-		private static void onServerDamageDealt(DamageReport report)
-		{
-			CharacterBody victim = report.victimBody;
-			if (!victim) return;
-			victim.SetBuffCount(ItemQualitiesContent.Buffs.PersonalShield.buffIndex, (int)(victim.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield) - report.damageDealt));
-		}
+        private static void onServerDamageDealt(DamageReport report)
+        {
+            CharacterBody victim = report.victimBody;
+            if (!victim)
+                return;
 
-		private static void OnInteractionsGlobal(Interactor interactor, IInteractable interactable, GameObject @object)
-		{
-			CharacterBody body = interactor.GetComponent<CharacterBody>();
-			if (!body) return;
-			float shieldPerInteract = 0;
-			switch(ItemQualitiesContent.ItemQualityGroups.PersonalShield.GetHighestQualityInInventory(body.master.inventory)) {
-				case QualityTier.Uncommon:
-					shieldPerInteract = 0.01f;
-					break;
-				case QualityTier.Rare:
-					shieldPerInteract = 0.02f;
-					break;
-				case QualityTier.Epic:
-					shieldPerInteract = 0.03f;
-					break;
-				case QualityTier.Legendary:
-					shieldPerInteract = 0.04f;
-					break;
-			}
-			ItemQualityCounts Qualities = ItemQualitiesContent.ItemQualityGroups.PersonalShield.GetItemCounts(body.master.inventory);
-			float MaxShield = Qualities.UncommonCount * 0.2f +
-							Qualities.RareCount * 0.5f +
-							Qualities.EpicCount * 0.9f +
-							Qualities.LegendaryCount * 2f;
+            victim.SetBuffCount(ItemQualitiesContent.Buffs.PersonalShield.buffIndex, (int)(victim.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield) - report.damageDealt));
+        }
 
-			body.SetBuffCount(ItemQualitiesContent.Buffs.PersonalShield.buffIndex, (int)Math.Min(body.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield) + shieldPerInteract * body.maxHealth, MaxShield * body.maxHealth));
-		}
+        private static void OnInteractionsGlobal(Interactor interactor, IInteractable interactable, GameObject @object)
+        {
+            if (!SharedItemUtils.InteractableIsPermittedForSpawn(interactable))
+                return;
 
-		private static void getStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
-		{
-			if (!sender) return;
-			args.baseShieldAdd += sender.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield);
-		}
-	}
+            CharacterBody body = interactor.GetComponent<CharacterBody>();
+            if (!body)
+                return;
+
+            ItemQualityCounts personalShield = ItemQualitiesContent.ItemQualityGroups.PersonalShield.GetItemCounts(body.inventory);
+            if (personalShield.TotalQualityCount <= 0)
+                return;
+
+            float shieldPerInteract;
+            switch (personalShield.HighestQuality)
+            {
+                case QualityTier.Uncommon:
+                    shieldPerInteract = 0.02f;
+                    break;
+                case QualityTier.Rare:
+                    shieldPerInteract = 0.05f;
+                    break;
+                case QualityTier.Epic:
+                    shieldPerInteract = 0.07f;
+                    break;
+                case QualityTier.Legendary:
+                    shieldPerInteract = 0.10f;
+                    break;
+                default:
+                    Log.Error($"Quality tier {personalShield.HighestQuality} is not implemented");
+                    return;
+            }
+
+            float maxShield = (personalShield.UncommonCount * 0.2f) +
+                              (personalShield.RareCount * 0.5f) +
+                              (personalShield.EpicCount * 0.9f) +
+                              (personalShield.LegendaryCount * 2f);
+
+            body.SetBuffCount(ItemQualitiesContent.Buffs.PersonalShield.buffIndex, (int)Math.Min(body.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield) + (shieldPerInteract * body.maxHealth), maxShield * body.maxHealth));
+        }
+
+        private static void getStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
+        {
+            if (!sender)
+                return;
+
+            args.baseShieldAdd += sender.GetBuffCount(ItemQualitiesContent.Buffs.PersonalShield);
+        }
+    }
 }
