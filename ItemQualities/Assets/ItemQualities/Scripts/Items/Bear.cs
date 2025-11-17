9f1d3aa6ba7f3dad8db13c7c7b441165fc8cefe1
Gorakh <petter.ohlen@gmail.com>
2025-11-17 20:13:33 +0100

    Tougher times rework

--- ItemQualities/Assets/ItemQualities/Scripts/Items/Bear.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/Bear.cs
@@ -1,4 +1,6 @@
-﻿using Mono.Cecil.Cil;
+﻿using ItemQualities.Utilities.Extensions;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using RoR2;
 using System;
@@ -15,38 +17,49 @@ namespace ItemQualities.Items
 
         static void HealthComponent_TakeDamageProcess(ILContext il)
         {
+            if (!il.Method.TryFindParameter<DamageInfo>(out ParameterDefinition damageInfoParameter))
+            {
+                Log.Error("Failed to find DamageInfo parameter");
+                return;
+            }
+
             ILCursor c = new ILCursor(il);
 
             if (!c.TryFindNext(out ILCursor[] foundCursors,
                                x => x.MatchLdfld<HealthComponent.ItemCounts>(nameof(HealthComponent.ItemCounts.bear)),
-                               x => x.MatchCallOrCallvirt(typeof(Util), nameof(Util.ConvertAmplificationPercentageIntoReductionPercentage))))
+                               x => x.MatchStfld<DamageInfo>(nameof(DamageInfo.rejected))))
             {
                 Log.Error("Failed to find patch location");
                 return;
             }
 
-            c.Goto(foundCursors[1].Next, MoveType.Before);
+            c.Goto(foundCursors[1].Next, MoveType.After); // stfld DamageInfo.rejected
 
             c.Emit(OpCodes.Ldarg_0);
-            c.EmitDelegate<Func<float, HealthComponent, float>>(getBearBlockChance);
+            c.Emit(OpCodes.Ldarg, damageInfoParameter);
+            c.EmitDelegate<Action<HealthComponent, DamageInfo>>(onBearBlockDamage);
 
-            static float getBearBlockChance(float blockChance, HealthComponent healthComponent)
+            static void onBearBlockDamage(HealthComponent healthComponent, DamageInfo damageInfo)
             {
                 CharacterBody body = healthComponent ? healthComponent.body : null;
                 Inventory inventory = body ? body.inventory : null;
 
-                ItemQualityCounts bear = default;
-                if (inventory)
+                ItemQualityCounts bear = ItemQualitiesContent.ItemQualityGroups.Bear.GetItemCounts(inventory);
+                if (bear.TotalQualityCount > 0)
                 {
-                    bear = ItemQualitiesContent.ItemQualityGroups.Bear.GetItemCounts(inventory);
+                    float damageFraction = damageInfo.damage / healthComponent.fullCombinedHealth;
+
+                    float invincibilityDurationPerPercentDamage = (0.01f * bear.UncommonCount) +
+                                                                  (0.05f * bear.RareCount) +
+                                                                  (0.15f * bear.EpicCount) +
+                                                                  (0.25f * bear.LegendaryCount);
+
+                    float invincibilityDuration = damageFraction * 100f * invincibilityDurationPerPercentDamage;
+                    if (invincibilityDuration >= 1f / 30f)
+                    {
+                        body.AddTimedBuff(RoR2Content.Buffs.Immune, invincibilityDuration);
+                    }
                 }
-
-                blockChance += ((20f - 15f) * bear.UncommonCount) +
-                               ((30f - 15f) * bear.RareCount) +
-                               ((40f - 15f) * bear.EpicCount) +
-                               ((50f - 15f) * bear.LegendaryCount);
-
-                return blockChance;
             }
         }
     }
