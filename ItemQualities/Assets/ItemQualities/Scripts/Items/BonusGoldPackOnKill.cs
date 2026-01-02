0a590e721dbd7c6b0148e74bdf8869f3e2372c5f
Gorakh <petter.ohlen@gmail.com>
2026-01-02 16:05:11 +0100

    Fix Ghor's tome buffs staying after item is removed
    Fix #116

--- ItemQualities/Assets/ItemQualities/Scripts/Items/BonusGoldPackOnKill.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/BonusGoldPackOnKill.cs
@@ -1,7 +1,5 @@
-﻿using ItemQualities.Utilities.Extensions;
-using R2API;
+﻿using R2API;
 using RoR2;
-using System;
 
 namespace ItemQualities.Items
 {
@@ -11,44 +9,6 @@ namespace ItemQualities.Items
         static void Init()
         {
             RecalculateStatsAPI.GetStatCoefficients += getStatCoefficients;
-            On.RoR2.CharacterMaster.UpdateMoneyBasedItems += giveBuffsOnGoldChange;
-        }
-
-        private static void giveBuffsOnGoldChange(On.RoR2.CharacterMaster.orig_UpdateMoneyBasedItems orig, CharacterMaster self)
-        {
-            orig(self);
-
-            CharacterBody body = self.GetBody();
-            if (!body || !self.inventory)
-                return;
-
-            ItemQualityCounts bonusGoldPackOnKill = self.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.BonusGoldPackOnKill);
-            if (bonusGoldPackOnKill.TotalQualityCount > 0)
-            {
-                float multiplier = 0;
-                switch (bonusGoldPackOnKill.HighestQuality)
-                {
-                    case QualityTier.Uncommon:
-                        multiplier = 2f;
-                        break;
-                    case QualityTier.Rare:
-                        multiplier = 3f;
-                        break;
-                    case QualityTier.Epic:
-                        multiplier = 3.5f;
-                        break;
-                    case QualityTier.Legendary:
-                        multiplier = 4f;
-                        break;
-                }
-
-                float max = (bonusGoldPackOnKill.UncommonCount * 20) +
-                            (bonusGoldPackOnKill.RareCount * 40) +
-                            (bonusGoldPackOnKill.EpicCount * 60) +
-                            (bonusGoldPackOnKill.LegendaryCount * 100);
-
-                body.SetBuffCount(ItemQualitiesContent.Buffs.GoldenGun.buffIndex, (int)Math.Min(multiplier * body.master.money / Run.instance.GetDifficultyScaledCost(25), max));
-            }
         }
 
         private static void getStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
---
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/BonusGoldPackOnKillQualityItemBehavior.cs
@@ -0,0 +1,112 @@
+﻿using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ItemQualities.Items
+{
+    public sealed class BonusGoldPackOnKillQualityItemBehavior : QualityItemBodyBehavior
+    {
+        [ItemGroupAssociation(QualityItemBehaviorUsageFlags.Server)]
+        static ItemQualityGroup GetItemGroup()
+        {
+            return ItemQualitiesContent.ItemQualityGroups.BonusGoldPackOnKill;
+        }
+
+        uint _lastGoldAmount;
+
+        float _buffRefreshTimer;
+
+        void OnEnable()
+        {
+            refreshBuff();
+            _lastGoldAmount = Body.master ? Body.master.money : 0;
+        }
+
+        void OnDisable()
+        {
+            if (NetworkServer.active)
+            {
+                for (int i = Body.GetBuffCount(ItemQualitiesContent.Buffs.GoldenGun); i > 0; i--)
+                {
+                    Body.RemoveBuff(ItemQualitiesContent.Buffs.GoldenGun);
+                }
+            }
+        }
+
+        void FixedUpdate()
+        {
+            _buffRefreshTimer -= Time.fixedDeltaTime;
+            if (_buffRefreshTimer <= 0f)
+            {
+                _buffRefreshTimer = 0.2f;
+
+                uint goldAmount = Body.master ? Body.master.money : 0;
+                if (goldAmount != _lastGoldAmount)
+                {
+                    _lastGoldAmount = goldAmount;
+                    refreshBuff();
+                }
+            }
+        }
+
+        void refreshBuff()
+        {
+            ItemQualityCounts bonusGoldPackOnKill = Stacks;
+
+            int maxBuffCount = (20 * bonusGoldPackOnKill.UncommonCount) +
+                               (40 * bonusGoldPackOnKill.RareCount) +
+                               (60 * bonusGoldPackOnKill.EpicCount) +
+                               (100 * bonusGoldPackOnKill.LegendaryCount);
+
+            float targetBuffCountMultiplier;
+            switch (bonusGoldPackOnKill.HighestQuality)
+            {
+                case QualityTier.None:
+                    targetBuffCountMultiplier = 0f;
+                    break;
+                case QualityTier.Uncommon:
+                    targetBuffCountMultiplier = 2f;
+                    break;
+                case QualityTier.Rare:
+                    targetBuffCountMultiplier = 3f;
+                    break;
+                case QualityTier.Epic:
+                    targetBuffCountMultiplier = 3.5f;
+                    break;
+                case QualityTier.Legendary:
+                    targetBuffCountMultiplier = 4f;
+                    break;
+                default:
+                    targetBuffCountMultiplier = 0f;
+                    Log.Error($"Quality tier {bonusGoldPackOnKill.HighestQuality} is not implemented");
+                    break;
+            }
+
+            uint money = Body.master ? Body.master.money : 0;
+            int moneyAmountPerBuff = Run.instance.GetDifficultyScaledCost(25, Stage.instance.entryDifficultyCoefficient);
+
+            int desiredBuffCount = (int)(targetBuffCountMultiplier * (money / (float)moneyAmountPerBuff));
+            int targetBuffCount = Mathf.Min(maxBuffCount, desiredBuffCount);
+
+            int currentBuffCount = Body.GetBuffCount(ItemQualitiesContent.Buffs.GoldenGun);
+
+            if (targetBuffCount != currentBuffCount)
+            {
+                if (targetBuffCount > currentBuffCount)
+                {
+                    for (int i = currentBuffCount; i < targetBuffCount; i++)
+                    {
+                        Body.AddBuff(ItemQualitiesContent.Buffs.GoldenGun);
+                    }
+                }
+                else // targetBuffCount < currentBuffCount
+                {
+                    for (int i = currentBuffCount; i > targetBuffCount; i--)
+                    {
+                        Body.RemoveBuff(ItemQualitiesContent.Buffs.GoldenGun);
+                    }
+                }
+            }
+        }
+    }
+}
