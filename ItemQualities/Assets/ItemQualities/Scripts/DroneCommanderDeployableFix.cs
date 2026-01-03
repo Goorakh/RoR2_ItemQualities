a49ab077ad2c71ded931f509f85476158d1b6951
Gorakh <petter.ohlen@gmail.com>
2026-01-03 14:22:24 +0100

    Fix drone parts drone spawning stopping at half capacity + Don't waste cooldown if drone fails to spawn

---
+++ ItemQualities/Assets/ItemQualities/Scripts/DroneCommanderDeployableFix.cs
@@ -0,0 +1,44 @@
+﻿using ItemQualities.Utilities.Extensions;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ItemQualities
+{
+    internal sealed class DroneCommanderDeployableFix : MonoBehaviour
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            if (DLC1Content.BodyPrefabs.DroneCommanderBody)
+            {
+                if (DLC1Content.BodyPrefabs.DroneCommanderBody.TryGetComponent(out Deployable deployable))
+                {
+                    DroneCommanderDeployableFix deployableFix = DLC1Content.BodyPrefabs.DroneCommanderBody.gameObject.AddComponent<DroneCommanderDeployableFix>();
+
+                    deployable.onUndeploy ??= new UnityEvent();
+                    deployable.onUndeploy.AddPersistentListener(deployableFix.OnUndeploy);
+                }
+                else
+                {
+                    Log.Error($"{DLC1Content.BodyPrefabs.DroneCommanderBody.name} is missing Deployable component");
+                }
+            }
+        }
+
+        CharacterBody _body;
+
+        void Awake()
+        {
+            _body = GetComponent<CharacterBody>();
+        }
+
+        public void OnUndeploy()
+        {
+            if (_body && _body.master)
+            {
+                _body.master.TrueKill();
+            }
+        }
+    }
+}
--- ItemQualities/Assets/ItemQualities/Scripts/Items/DroneWeapons.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/DroneWeapons.cs
@@ -1,6 +1,5 @@
 using ItemQualities.Utilities.Extensions;
 using RoR2;
-using UnityEngine;
 
 namespace ItemQualities.Items
 {
@@ -10,8 +9,6 @@ namespace ItemQualities.Items
         static void Init()
         {
             On.RoR2.CharacterMaster.GetDeployableSameSlotLimit += CharacterMaster_GetDeployableSameSlotLimit;
-            On.RoR2.DroneWeaponsBehavior.FixedUpdate += DroneWeaponsBehavior_FixedUpdate;
-            On.RoR2.DroneWeaponsBehavior.TrySpawnDrone += DroneWeaponsBehavior_TrySpawnDrone;
             On.RoR2.DroneWeaponsBehavior.OnMasterSpawned += DroneWeaponsBehavior_OnMasterSpawned;
         }
 
@@ -19,60 +16,31 @@ namespace ItemQualities.Items
         {
             orig(self, spawnResult);
 
-            if (!self.body)
+            if (!self.body || !self.body.master)
                 return;
 
-            ItemQualityCounts droneWeapons = default;
-            if (self.body.inventory)
-            {
-                droneWeapons = self.body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.DroneWeapons);
-            }
-
-            if (droneWeapons.TotalQualityCount > 0)
+            if (!self.body.master.IsDeployableLimited(DeployableSlot.DroneDynamiteDrone))
             {
                 self.hasSpawnedDrone = false;
-            }
 
-            GameObject spawnedInstance = spawnResult.spawnedInstance;
-            if (!spawnedInstance)
-                return;
-
-            CharacterMaster droneMaster = spawnedInstance.GetComponent<CharacterMaster>();
-            if (!droneMaster)
-                return;
-
-            GameObject droneBodyObject = droneMaster.bodyInstanceObject;
-            if (!droneBodyObject)
-                return;
-
-            Deployable deployable = droneBodyObject.GetComponent<Deployable>();
-            if (deployable)
-            {
-                self.body.master.AddDeployable(deployable, DeployableSlot.DroneWeaponsDrone);
-            }
-        }
-
-        static void DroneWeaponsBehavior_TrySpawnDrone(On.RoR2.DroneWeaponsBehavior.orig_TrySpawnDrone orig, DroneWeaponsBehavior self)
-        {
-            orig(self);
-
-            if (self.body && self.body.inventory)
-            {
-                ItemQualityCounts droneWeapons = self.body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.DroneWeapons);
-                switch (droneWeapons.HighestQuality)
+                if (spawnResult.success && self.body.inventory)
                 {
-                    case QualityTier.Uncommon:
-                        self.spawnDelay = 120f;
-                        break;
-                    case QualityTier.Rare:
-                        self.spawnDelay = 90f;
-                        break;
-                    case QualityTier.Epic:
-                        self.spawnDelay = 45f;
-                        break;
-                    case QualityTier.Legendary:
-                        self.spawnDelay = 1f;
-                        break;
+                    ItemQualityCounts droneWeapons = self.body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.DroneWeapons);
+                    switch (droneWeapons.HighestQuality)
+                    {
+                        case QualityTier.Uncommon:
+                            self.spawnDelay = 120f;
+                            break;
+                        case QualityTier.Rare:
+                            self.spawnDelay = 90f;
+                            break;
+                        case QualityTier.Epic:
+                            self.spawnDelay = 45f;
+                            break;
+                        case QualityTier.Legendary:
+                            self.spawnDelay = 1f;
+                            break;
+                    }
                 }
             }
         }
@@ -87,39 +55,14 @@ namespace ItemQualities.Items
 
                 if (droneWeapons.TotalQualityCount > 0)
                 {
-                    result += (droneWeapons.UncommonCount * 2) +
-                              (droneWeapons.RareCount * 3) +
-                              (droneWeapons.EpicCount * 4) +
-                              (droneWeapons.LegendaryCount * 5) - 1;
+                    result += (2 * droneWeapons.UncommonCount) +
+                              (3 * droneWeapons.RareCount) +
+                              (4 * droneWeapons.EpicCount) +
+                              (5 * droneWeapons.LegendaryCount) - 1;
                 }
             }
 
             return result;
-        }
-
-        static void DroneWeaponsBehavior_FixedUpdate(On.RoR2.DroneWeaponsBehavior.orig_FixedUpdate orig, DroneWeaponsBehavior self)
-        {
-            orig(self);
-
-            if (self.body && self.body.inventory && self.body.master && self.body.master.IsDeployableLimited(DeployableSlot.DroneWeaponsDrone))
-            {
-                ItemQualityCounts droneWeapons = self.body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.DroneWeapons);
-                switch (droneWeapons.HighestQuality)
-                {
-                    case QualityTier.Uncommon:
-                        self.spawnDelay = 120f;
-                        break;
-                    case QualityTier.Rare:
-                        self.spawnDelay = 90f;
-                        break;
-                    case QualityTier.Epic:
-                        self.spawnDelay = 45f;
-                        break;
-                    case QualityTier.Legendary:
-                        self.spawnDelay = 1f;
-                        break;
-                }
-            }
         }
     }
 }
