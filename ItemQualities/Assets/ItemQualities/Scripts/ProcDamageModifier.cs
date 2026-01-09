75ce32c16f3435bf0d9865a9a77922069f140997
Gorakh <petter.ohlen@gmail.com>
2026-01-09 21:50:40 +0100

    Prevent procs and non-skill damage for Network Crystals and shootable drones
    Fix #144

---
+++ ItemQualities/Assets/ItemQualities/Scripts/ProcDamageModifier.cs
@@ -0,0 +1,18 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace ItemQualities
+{
+    public sealed class ProcDamageModifier : MonoBehaviour, IOnIncomingDamageServerReceiver
+    {
+        public float ProcCoefficientMultiplier = 1f;
+
+        public DamageTypeCombo DamageTypeToAdd;
+
+        public void OnIncomingDamageServer(DamageInfo damageInfo)
+        {
+            damageInfo.procCoefficient *= ProcCoefficientMultiplier;
+            damageInfo.damageType |= DamageTypeToAdd;
+        }
+    }
+}
