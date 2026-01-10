8df703c0c1ea5027e548b69e1e0369aea068d41e
Gorakh <petter.ohlen@gmail.com>
2026-01-10 19:46:52 +0100

    Fix adding duplicate crafting recipes for quality items
    Fix #111

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Utilities/CollectionComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ItemQualities.Utilities
+{
+    internal sealed class CollectionComparer<T> : IComparer<ICollection<T>>
+    {
+        public static CollectionComparer<T> SizeAscending { get; } = new CollectionComparer<T>(false);
+
+        public static CollectionComparer<T> SizeDescending { get; } = new CollectionComparer<T>(true);
+
+        readonly bool _descending;
+
+        private CollectionComparer(bool descending)
+        {
+            _descending = descending;
+        }
+
+        public int Compare(ICollection<T> x, ICollection<T> y)
+        {
+            int xSize = x != null ? x.Count : -1;
+            int ySize = y != null ? y.Count : -1;
+
+            return _descending ? ySize - xSize : xSize - ySize;
+        }
+    }
+}
