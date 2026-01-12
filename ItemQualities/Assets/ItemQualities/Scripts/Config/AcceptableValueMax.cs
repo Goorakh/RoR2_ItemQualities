da3e793d9ee12bcffc759fe4cc74830bb169644f
Gorakh <petter.ohlen@gmail.com>
2026-01-12 21:34:04 +0100

    Make difficulty changes into slider
    Fix #125

---
+++ ItemQualities/Assets/ItemQualities/Scripts/Config/AcceptableValueMax.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx.Configuration;
+using System;
+
+namespace ItemQualities.Config
+{
+    public sealed class AcceptableValueMax<T> : AcceptableValueBase where T : IComparable
+    {
+        public T MaxValue { get; }
+
+        readonly string _cachedMaxValueString;
+
+        public AcceptableValueMax(T maxValue, string customValueFormat = null) : base(typeof(T))
+        {
+            if (maxValue == null)
+            {
+                throw new ArgumentNullException(nameof(maxValue));
+            }
+
+            MaxValue = maxValue;
+
+            _cachedMaxValueString = string.IsNullOrEmpty(customValueFormat) ? MaxValue.ToString() : string.Format("{0:" + customValueFormat + "}", MaxValue);
+        }
+
+        public override object Clamp(object value)
+        {
+            if (MaxValue.CompareTo(value) < 0)
+            {
+                return MaxValue;
+            }
+
+            return value;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return MaxValue.CompareTo(value) >= 0;
+        }
+
+        public override string ToDescriptionString()
+        {
+            return $"# Acceptable value range: Lesser than or equal to {_cachedMaxValueString}";
+        }
+    }
+}
