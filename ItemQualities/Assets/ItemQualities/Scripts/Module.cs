6b57001ff70544215608c22d4ba9dee4fd0d8686
Gorakh <petter.ohlen@gmail.com>
2026-02-18 15:42:56 +0100

    Draft progress

--- ItemQualities/Assets/ItemQualities/Scripts/Module.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Module.cs
@@ -1,16 +1,37 @@
 ﻿using HG.Reflection;
+using ItemQualities.Utilities;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace ItemQualities
 {
-    internal abstract class Module
+    public abstract class Module<T> : IModule, IDisposable where T : Module<T>, new()
     {
-        protected virtual Type[] requiredModuleTypes { get; } = Array.Empty<Type>();
+        static T _instance;
+        public static T Instance => _instance;
 
-        protected virtual bool ShouldLoad()
+        protected Module()
         {
-            return true;
+            if (this is not T thisT)
+                throw new ArgumentException($"Module type {GetType().FullName} must be assignable to singleton type {typeof(T).FullName}");
+
+            if (GenericSingletonHelper.Assign(ref _instance, thisT))
+            {
+                OnEnable();
+            }
         }
+
+        public void Dispose()
+        {
+            if (this is T thisT && GenericSingletonHelper.Unassign(ref _instance, thisT))
+            {
+                OnDisable();
+            }
+        }
+
+        void IModule.OnEnable() => OnEnable();
+
+        void IModule.OnDisable() => OnDisable();
 
         protected virtual void OnEnable()
         {
@@ -21,9 +42,51 @@ namespace ItemQualities
         }
     }
 
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    internal sealed class ModuleInfoAttribute : SearchableAttribute
+    public interface IModule
     {
+        void OnEnable()
+        {
+        }
 
+        void OnDisable()
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public sealed class ModuleInfoAttribute : SearchableAttribute
+    {
+        public Type ModuleType
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (Type)target;
+        }
+
+        public Type[] Dependencies { get; init; } = Array.Empty<Type>();
+
+        public ModuleInfoAttribute(string moduleName)
+        {
+
+        }
+
+        public bool IsEnabled()
+        {
+            foreach (Type dependencyType in Dependencies)
+            {
+
+            }
+
+            return true;
+        }
+    }
+
+    [ModuleInfo(Dependencies = new Type[] { typeof(Test2Module) })]
+    public sealed class TestModule : Module<TestModule>
+    {
+    }
+
+    public sealed class Test2Module : Module<TestModule>
+    {
     }
 }
---
+++ ItemQualities/Assets/ItemQualities/Scripts/Utilities/GenericSingletonHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ItemQualities.Utilities
+{
+    static class GenericSingletonHelper
+    {
+        public static bool Assign<T>(ref T instance, T value) where T : class
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (instance == null)
+            {
+                instance = value;
+                return true;
+            }
+
+            Log.Error($"Duplicate instance of singleton class {typeof(T).FullName}");
+            return false;
+        }
+
+        public static T Assign<T>(T existingInstance, T value) where T : class
+        {
+            Assign(ref existingInstance, value);
+            return existingInstance;
+        }
+
+        public static bool Unassign<T>(ref T field, T instance) where T : class
+        {
+            if (instance is null)
+                throw new ArgumentNullException(nameof(instance));
+
+            if (ReferenceEquals(field, instance))
+            {
+                field = null;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static T Unassign<T>(T existingInstance, T instance) where T : class
+        {
+            Unassign(ref existingInstance, instance);
+            return existingInstance;
+        }
+    }
+}
