7a660b1faa05b36751e7ee867688aed54e143117
Gorakh <petter.ohlen@gmail.com>
2026-05-24 18:10:47 +0200

    Optimize unnecessary sprite mipmap and physics shape settings

--- ItemQualities/Assets/ItemQualities/Scripts/ItemQualityGroup.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/ItemQualityGroup.cs
@@ -415,6 +415,12 @@ namespace ItemQualities
                         textureImporter.spritePixelsPerUnit = qualityIconTexture.width / 5.12f;
                         textureImporter.alphaIsTransparency = true;
 
+                        TextureImporterSettings textureImporterSettings = new TextureImporterSettings();
+                        textureImporter.ReadTextureSettings(textureImporterSettings);
+                        textureImporterSettings.spriteGenerateFallbackPhysicsShape = false;
+                        textureImporterSettings.mipmapEnabled = false;
+                        textureImporter.SetTextureSettings(textureImporterSettings);
+
                         AssetDatabase.ImportAsset(qualityIconTextureAssetPath, ImportAssetOptions.ForceUpdate);
 
                         itemDef.pickupIconSprite = AssetDatabase.LoadAssetAtPath<Sprite>(qualityIconTextureAssetPath);
