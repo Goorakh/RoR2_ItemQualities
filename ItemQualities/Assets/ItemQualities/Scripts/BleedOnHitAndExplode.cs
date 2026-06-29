6f6cb3b4f5f6efeaa9d26b66a279569cf94e3aff
Onyx <50289874+Tizitendo@users.noreply.github.com>
2026-06-29 07:23:57 +0200

    rework

--- ItemQualities/Assets/ItemQualities/Scripts/BleedOnHitAndExplode.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/BleedOnHitAndExplode.cs
@@ -1,92 +1,153 @@
 using ItemQualities.Utilities;
 using ItemQualities.Utilities.Extensions;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using MonoMod.Cil;
 using RoR2;
+using RoR2.Projectile;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace ItemQualities.Items
 {
     public static class BleedOnHitAndExplode
     {
-        [SystemInitializer]
+        static EquipmentIndex[] _bleedEquipments = Array.Empty<EquipmentIndex>();
+
+        [SystemInitializer(typeof(QualityCatalog))]
         static void Init() 
         {
-            IL.RoR2.GlobalEventManager.ProcessHitEnemy += ProcerssHitEnemy;
+            HashSet<EquipmentIndex> bleedEquipments = new HashSet<EquipmentIndex>(EquipmentCatalog.equipmentCount*(int)QualityTier.Count);
+            for (QualityTier qualityTier = QualityTier.None; qualityTier < QualityTier.Count; qualityTier++)
+            {
+                void tryAddEquipment(EquipmentQualityGroup equipmentGroup)
+                {
+                    if (!equipmentGroup)
+                        return;
+
+                    EquipmentIndex equipmentIndex = equipmentGroup.GetEquipmentIndex(qualityTier);
+                    Debug.Log(qualityTier);
+                    if (equipmentIndex != EquipmentIndex.None)
+                    {
+                        bleedEquipments.Add(equipmentIndex);
+                    }
+                }
+                tryAddEquipment(ItemQualitiesContent.EquipmentQualityGroups.Saw);
+            }
+
+            if (bleedEquipments.Count > 0)
+            {
+                _bleedEquipments = bleedEquipments.ToArray();
+                Array.Sort(_bleedEquipments);
+            }
+
+            On.RoR2.GlobalEventManager.ProcessHitEnemy += ProcessHitEnemy;
+            On.RoR2.DotController.InflictDot_refInflictDotInfo += DotController_InflictDot_refInflictDotInfo;
         }
 
-        private static void ProcerssHitEnemy(ILContext il)
+        private static void DotController_InflictDot_refInflictDotInfo(On.RoR2.DotController.orig_InflictDot_refInflictDotInfo orig, ref InflictDotInfo inflictDotInfo)
         {
-            if (!il.Method.TryFindParameter<DamageInfo>(out ParameterDefinition damageInfoParameter))
+            try
             {
-                Log.Error("Failed to find damageInfo parameter");
-                return;
-            }
-
-            ILCursor c = new ILCursor(il);
-
-            if (!c.TryGotoNext(
-            x => x.MatchLdsfld(typeof(RoR2Content.Items), nameof(RoR2Content.Items.BleedOnHitAndExplode)))
-            || !c.TryGotoNext(
-            x => x.MatchCallOrCallvirt(typeof(DotController), nameof(DotController.InflictDot)))
-            || !c.TryGotoPrev(MoveType.After,
-            x => x.MatchLdcI4((int)DotController.DotIndex.Bleed)))
-            {
-                Log.Error("Failed to find patch location");
-                return;
-            }
-
-            c.Emit(OpCodes.Ldarg, damageInfoParameter);
-            c.EmitDelegate<Func<DotController.DotIndex, DamageInfo, DotController.DotIndex>>(UpgradeBleed);
-
-            DotController.DotIndex UpgradeBleed(DotController.DotIndex bleedIndex, DamageInfo damageInfo)
-            {
-                if (!damageInfo.attacker)
-                    return bleedIndex;
-                if (!damageInfo.attacker.TryGetComponent(out CharacterBody body) || !body.inventory || !body.master)
-                    return bleedIndex;
-                if (!damageInfo.crit)
-                    return bleedIndex;
-
-                ItemQualityCounts bleedOnHitAndExplode = body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.BleedOnHitAndExplode);
-                if (bleedOnHitAndExplode.TotalQualityCount == 0)
-                    return bleedIndex;
-                float upgradeChance =   bleedOnHitAndExplode.UncommonCount * 5 +
-                                        bleedOnHitAndExplode.RareCount * 10 +
-                                        bleedOnHitAndExplode.EpicCount * 15 +
-                                        bleedOnHitAndExplode.LegendaryCount * 25;
-
-                if (RollUtil.CheckRoll(upgradeChance, body.master, damageInfo.procChainMask.HasProc(ProcType.SureProc)))
+                if (inflictDotInfo.dotIndex == DotController.DotIndex.SuperBleed)
                 {
-                    return DotController.DotIndex.SuperBleed;
-                }
-                else
-                {
-                    return bleedIndex;
+                    CharacterBody attackerBody = inflictDotInfo.attackerObject ? inflictDotInfo.attackerObject.GetComponent<CharacterBody>() : null;
+                    Inventory attackerInventory = attackerBody ? attackerBody.inventory : null;
+                    if (attackerInventory && attackerBody.master)
+                    {
+                        ItemQualityCounts bleedOnHitAndExplode = attackerInventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.BleedOnHitAndExplode);
+
+                        float damageMult =  bleedOnHitAndExplode.UncommonCount * 0.03f +
+                                            bleedOnHitAndExplode.RareCount * 0.06f +
+                                            bleedOnHitAndExplode.EpicCount * 0.1f +
+                                            bleedOnHitAndExplode.LegendaryCount * 0.15f;
+
+                        inflictDotInfo.damageMultiplier += ((1 + damageMult) * getBleedCount(attackerBody.master));
+                    }
                 }
             }
-
-            c.Emit(OpCodes.Dup);
-
-            if (!c.TryGotoNext(MoveType.After,
-            x => x.MatchLdfld(typeof(DamageInfo), nameof(DamageInfo.procCoefficient)),
-            x => x.MatchMul()))
+            catch (Exception e)
             {
-                Log.Error("Failed to find patch location");
+                Log.Error_NoCallerPrefix(e.ToString());
+            }
+
+            orig(ref inflictDotInfo);
+        }
+
+        static void ProcessHitEnemy(On.RoR2.GlobalEventManager.orig_ProcessHitEnemy orig, GlobalEventManager self, DamageInfo damageInfo, GameObject victim)
+        {
+            orig(self, damageInfo, victim);
+            if (!damageInfo.attacker)
                 return;
+            if (!damageInfo.attacker.TryGetComponent(out CharacterBody body) || !body.inventory || !body.master)
+                return;
+            if (!damageInfo.crit)
+                return;
+
+            ItemQualityCounts bleedOnHitAndExplode = body.inventory.GetItemCountsEffective(ItemQualitiesContent.ItemQualityGroups.BleedOnHitAndExplode);
+            if (bleedOnHitAndExplode.TotalQualityCount == 0)
+                return;
+
+            uint? maxStacksFromAttacker = null;
+            if (damageInfo?.inflictor)
+            {
+                ProjectileDamage projectileDamage = damageInfo.inflictor.GetComponent<ProjectileDamage>();
+                if (projectileDamage && projectileDamage.useDotMaxStacksFromAttacker)
+                {
+                    maxStacksFromAttacker = projectileDamage.dotMaxStacksFromAttacker;
+                }
             }
 
-            c.Emit(OpCodes.Ldarg, damageInfoParameter);
-            c.EmitDelegate<Func<DotController.DotIndex, float, DamageInfo, float>>(IncreaseDuration);
-            float IncreaseDuration(DotController.DotIndex dotIndex, float duration, DamageInfo damageInfo)
+            float hemorrhageChance = bleedOnHitAndExplode.HighestQuality switch
             {
-                if (dotIndex == DotController.DotIndex.SuperBleed)
-                {
-                    duration += 13 * damageInfo.procCoefficient;
-                }
-                return duration;
+                QualityTier.Uncommon => 5f,
+                QualityTier.Rare => 10f,
+                QualityTier.Epic => 15f,
+                QualityTier.Legendary => 20f,
+                _ => 0
+            };
+
+            if (RollUtil.CheckRoll(hemorrhageChance, body.master, damageInfo.procChainMask.HasProc(ProcType.SureProc)))
+            {
+                DotController.InflictDot(victim, damageInfo.attacker, damageInfo.inflictedHurtbox, DotController.DotIndex.SuperBleed, 15f * damageInfo.procCoefficient, 1f, maxStacksFromAttacker);
             }
+        }
+
+        static int getBleedCount(CharacterMaster master)
+        {
+            int bleedItemCount = 0;
+
+            if (!master.inventory.inventoryDisabled)
+            {
+                foreach (ItemIndex itemIndex in ItemCatalog.GetItemsWithTag(ItemTags.BleedRelated))
+                {
+                    bleedItemCount += master.inventory.GetItemCountEffective(itemIndex);
+                }
+            }
+
+            if (!master.inventory.GetEquipmentDisabled())
+            {
+                int equipmentSlotCount = master.inventory.GetEquipmentSlotCount();
+                for (uint slot = 0; slot < equipmentSlotCount; slot++)
+                {
+                    int equipmentSetCount = master.inventory.GetEquipmentSetCount(slot);
+                    for (uint set = 0; set < equipmentSetCount; set++)
+                    {
+                        EquipmentState equipmentState = master.inventory.GetEquipment(slot, set);
+                        if (equipmentState.equipmentIndex != EquipmentIndex.None &&
+                            Array.BinarySearch(_bleedEquipments, equipmentState.equipmentIndex) >= 0)
+                        {
+                            bleedItemCount++;
+                        }
+                    }
+                }
+            }
+
+            if (master && master.minionOwnership.ownerMaster)
+            {
+                bleedItemCount += getBleedCount(master.minionOwnership.ownerMaster);
+            }
+
+            return bleedItemCount;
         }
     }
 }
--- ItemQualities/Assets/ItemQualities/Scripts/ItemTags.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/ItemTags.cs
@@ -8,11 +8,13 @@ namespace ItemQualities
     public static class ItemTags
     {
         public static ItemTag MissileRelated { get; private set; } = (ItemTag)(-1);
+        public static ItemTag BleedRelated { get; private set; } = (ItemTag)(-1);
 
         [InitDuringStartupPhase(GameInitPhase.PreFrame)]
         static void PreInit()
         {
             MissileRelated = ItemAPI.AddItemTag("Quality_MissileRelated");
+            BleedRelated = ItemAPI.AddItemTag("Quality_BleedRelated");
 
             static void addTags(string itemDefAssetGuid, params ItemTag[] tags)
             {
@@ -36,6 +38,11 @@ namespace ItemQualities
             addTags(RoR2_DLC1_MoreMissile.MoreMissile_asset, MissileRelated);
 
             addTags(RoR2_DLC2_Items_BarrageOnBoss.BarrageOnBoss_asset, MissileRelated);
+
+            addTags(RoR2_Base_BleedOnHit.BleedOnHit_asset, BleedRelated);
+            addTags(RoR2_DLC2_Items_TriggerEnemyDebuffs.TriggerEnemyDebuffs_asset, BleedRelated);
+            addTags(RoR2_DLC2_Items_TeleportOnLowHealth.TeleportOnLowHealth_asset, BleedRelated);
+            addTags(RoR2_Base_BleedOnHitAndExplode.BleedOnHitAndExplode_asset, BleedRelated);
         }
     }
 }
