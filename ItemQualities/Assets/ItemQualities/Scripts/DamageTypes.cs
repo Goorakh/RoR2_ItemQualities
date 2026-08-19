82b7fe153bf10397e3bc308dbebda796b7946dc4
Gorakh <petter.ohlen@gmail.com>
2026-08-19 22:42:51 +0200

    Forgot to commit DamageType for quality Queens Gland

--- ItemQualities/Assets/ItemQualities/Scripts/DamageTypes.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/DamageTypes.cs
@@ -16,6 +16,8 @@ namespace ItemQualities
     {
         public static DamageAPI.ModdedDamageType Frost6s { get; private set; }
 
+        public static DamageAPI.ModdedDamageType ProcOnly { get; private set; }
+
         public static DamageAPI.ModdedDamageType ForceAddToSharedSuffering { get; private set; }
 
         public static DamageAPI.ModdedDamageType BypassDrops { get; private set; }
@@ -31,6 +33,7 @@ namespace ItemQualities
         private static void Init()
         {
             Frost6s = DamageAPI.ReserveDamageType();
+            ProcOnly = DamageAPI.ReserveDamageType();
             ForceAddToSharedSuffering = DamageAPI.ReserveDamageType();
             BypassDrops = DamageAPI.ReserveDamageType();
             Echo = DamageAPI.ReserveDamageType();
@@ -38,6 +41,8 @@ namespace ItemQualities
             Lifesteal50 = DamageAPI.ReserveDamageType();
 
             GlobalEventManager.onServerDamageDealt += onServerDamageDealt;
+
+            IL.RoR2.HealthComponent.TakeDamageProcess += HealthComponent_TakeDamageProcess_ProcOnlyPatch;
 
             IL.RoR2.GlobalEventManager.OnCharacterDeath += GlobalEventManager_OnCharacterDeath;
 
@@ -106,6 +111,52 @@ namespace ItemQualities
                         attackerBody.healthComponent.Heal(damageReport.damageDealt * lifestealCoefficient, new ProcChainMask());
                     }
                 }
+            }
+        }
+
+        private static void HealthComponent_TakeDamageProcess_ProcOnlyPatch(ILContext il)
+        {
+            if (!il.Method.TryFindParameter<DamageInfo>(out ParameterDefinition damageInfoParameter))
+            {
+                Log.Error("Failed to find DamageInfo parameter");
+                return;
+            }
+
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryFindNext(out ILCursor[] foundCursors,
+                               x => x.MatchLdsfld(typeof(RoR2Content.Buffs), nameof(RoR2Content.Buffs.PermanentCurse)),
+                               x => x.MatchCallOrCallvirt<HealthComponent>(nameof(HealthComponent.TakeDamageForce))))
+            {
+                Log.Error("Failed to find patch end location");
+                return;
+            }
+
+            c.Goto(foundCursors[0].Next, MoveType.Before); // ldsfld RoR2Content.Buffs.PermanentCurse
+
+            ILLabel startHurtBlockLabel = default;
+            if (!c.TryGotoPrev(x => x.MatchLdfld<DamageInfo>(nameof(DamageInfo.delayedDamageSecondHalf))) ||
+                !c.TryGotoNext(x => x.MatchBrtrue(out startHurtBlockLabel)))
+            {
+                Log.Error("Failed to find patch start location");
+                return;
+            }
+
+            c.Goto(foundCursors[1].Next, MoveType.After); // call HealthComponent.TakeDamageForce
+            c.MoveAfterLabels();
+            ILLabel endHurtBlockLabel = c.MarkLabel();
+
+            c.Goto(startHurtBlockLabel.Target, MoveType.AfterLabel);
+
+            c.Emit(OpCodes.Ldarg, damageInfoParameter);
+            c.EmitDelegate<Func<DamageInfo, bool>>(shouldDealDamage);
+            c.Emit(OpCodes.Brfalse, endHurtBlockLabel);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static bool shouldDealDamage(DamageInfo damageInfo)
+            {
+                bool isProcOnly = damageInfo != null && damageInfo.HasModdedDamageType(ProcOnly);
+                return !isProcOnly;
             }
         }
 
