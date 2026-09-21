73a37ff46a733f01948245450f385137ce041fe8
Onyx <50289874+Tizitendo@users.noreply.github.com>
2026-09-21 17:19:02 +0200

    pr fixes

--- ItemQualities/Assets/ItemQualities/Scripts/Items/ExecuteLowHealthElite.cs
+++ ItemQualities/Assets/ItemQualities/Scripts/Items/ExecuteLowHealthElite.cs
@@ -1,28 +1,20 @@
 ﻿using ItemQualities.ModCompatibility;
-using ItemQualities.Utilities;
 using ItemQualities.Utilities.Extensions;
 using R2API;
 using RoR2;
-using RoR2BepInExPack.GameAssetPathsBetter;
 using UnityEngine;
 
 namespace ItemQualities.Items
 {
     internal static class ExecuteLowHealthElite
     {
-        private static BodyIndex _crabJointBodyIndex;
+        private static BodyIndex _crabJointBodyIndex = BodyIndex.None;
 
-        [SystemInitializer]
+        [SystemInitializer(typeof(BodyCatalog))]
         private static void Init()
         {
             ExecuteAPI.CalculateExecuteThresholdForViewerBypassImmunity += calculateExecuteThreshold;
-            AddressableUtil.LoadAssetAsync<GameObject>(RoR2_DLC1_VoidRaidCrab.VoidRaidCrabJointBody_prefab).OnSuccess(crabJointPrefab =>
-            {
-                if (crabJointPrefab.TryGetComponent(out CharacterBody body))
-                {
-                    _crabJointBodyIndex = body.bodyIndex;
-                }
-            });
+            _crabJointBodyIndex = BodyCatalog.FindBodyIndex("VoidRaidCrabJointBody");
         }
 
         private static void calculateExecuteThreshold(CharacterBody victimBody, CharacterBody viewerBody, ref float highestExecuteThreshold)
