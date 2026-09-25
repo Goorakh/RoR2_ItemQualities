1fccdacb64ac9ca7d52014141e482847839b8ca5
Onyx <50289874+Tizitendo@users.noreply.github.com>
2026-09-25 10:25:17 +0200

    removed fathomless compat

--- ItemQualities/Assets/ItemQualities/Scripts/ModCompatibility/FathomlessCompat.cs
+++
@@ -1,9 +0,0 @@
-﻿using BepInEx.Bootstrap;
-
-namespace ItemQualities.ModCompatibility
-{
-    internal static class FathomlessCompat
-    {
-        public static bool Enabled => Chainloader.PluginInfos.ContainsKey("Nuxlar.FathomlessVoidling");
-    }
-}
